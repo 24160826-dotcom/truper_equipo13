e0b3cb43c23bfbf3c88e89d9fab339597f9b11a2
Kevin <24160826@itoaxaca.edu.mx>
2026-06-01 15:06:05 +0000

    boton normal

--- index.php
+++ index.php
@@ -132,7 +132,7 @@
         profesional de herramientas, productos y refacciones.
     </p>
 
-    <a href="login.php" class="btn">Panel control kevin</a>
+    <a href="login.php" class="btn">Inicar sesion</a>
 </section>
 
 <section class="cards" id="productos">
