db25b936ef7a720906c864446ab536815cf89e14
Kevin <24160826@itoaxaca.edu.mx>
2026-06-03 16:52:01 +0000

    login tercera oportunidad 4sc SMKG

--- login.php
+++ login.php
@@ -152,7 +152,7 @@ body::before{
 <div class="login-box">
 
     <div class="logo">
-        Tercera oportuinidad 4sc SMKG
+       Tercera oportunidad 4sc SMKG
     </div>
 
     <h1 class="titulo">
